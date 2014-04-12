9750f9307264557223f528cb5d3e4bc240749bd7
Michal Dorner <dorner.michal@gmail.com>
2014-04-12 19:25:17 +0200

    updated to XJade@0.2.1

--- test/expected/client.js
+++ test/expected/client.js
@@ -1,5 +1,6 @@
 function (parent) {
     var __expr;
+    if (!parent) parent=document.createDocumentFragment();
     /* LINE: 2 */
     var div$1 = document.createElement('div');
     div$1.textContent = "client template";
